8f3f77665eb57f86208588369c03ebc0cd0ab5e5
Gabriel <developer.gabriel@gmail.com>
2015-08-05 16:14:25 -0300

    first commit

---
+++ EmptyTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CdcAPI
+{
+    public class EmptyTask
+    {
+        public static Task Start()
+        {
+            var taskSource = new TaskCompletionSource<AsyncVoid>();
+            taskSource.SetResult(default(AsyncVoid));
+            return taskSource.Task as Task;
+        }
+
+        private struct AsyncVoid
+        {
+        }
+    }
+}
---
+++ NakedBodyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace CdcAPI
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public sealed class NakedBodyAttribute : ParameterBindingAttribute
+    {
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentException("Invalid parameter");
+
+            return new NakedBodyParameterBinding(parameter);
+        }
+    }
+}
